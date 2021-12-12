c33bbfc6a1a33ca263a9f54d3673515b29b6fc20
stambolievv <deyan.stamboliev@gmail.com>
2021-12-13 01:49:07 +0200

    add linear gradient

--- src/models/Platform.js
+++ src/models/Platform.js
@@ -8,8 +8,8 @@ export default class Platform {
     draw(ctx) {
         ctx.beginPath();
         const gradient = ctx.createLinearGradient(0, 0, 0, ctx.canvas.height);
-        gradient.addColorStop(0, 'lightgreen');
-        gradient.addColorStop(1, 'darkgreen');
+        gradient.addColorStop(0, 'LightGreen');
+        gradient.addColorStop(1, 'DarkGreen');
         ctx.fillStyle = gradient;
         // ctx.fillStyle = 'hsl(10, 50%, 50%, 0.5)';
         ctx.rect(this.pos.x, this.pos.y, this.dim.w, this.dim.h);
