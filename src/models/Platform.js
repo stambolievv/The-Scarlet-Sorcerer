7a62c8b7d4b67117dfbc6dbfa56d1801fac45eb4
stambolievv <deyan.stamboliev@gmail.com>
2021-12-14 16:16:57 +0200

    add debug text for each tile

--- src/models/Platform.js
+++ src/models/Platform.js
@@ -1,17 +1,20 @@
 export default class Platform {
-    constructor(tilesheet, sourceX, sourceY, destinationX, destinationY, frameWidth, frameHeight) {
+    constructor(tilesheet, sourceX, sourceY, destinationX, destinationY, frameWidth, frameHeight, tileValue) {
         this.sprite = tilesheet;
         this.src = { x: sourceX, y: sourceY };
         this.pos = { x: destinationX, y: destinationY };
         this.dim = { w: frameWidth, h: frameHeight };
+        this.tileValue = tileValue;
     }
 
     draw(ctx) {
-
         ctx.drawImage(this.sprite, this.src.x, this.src.y, this.dim.w, this.dim.h, this.pos.x, this.pos.y, this.dim.w, this.dim.h);
 
         if (ctx.DEBUG) {
             ctx.beginPath();
+            ctx.fillStyle = 'yellow';
+            ctx.font = 'bold 24px serif';
+            ctx.fillText(this.tileValue, this.pos.x + this.dim.w / 2, this.pos.y + this.dim.h / 2);
             ctx.fillStyle = 'black';
             ctx.lineWidth = 2;
             ctx.strokeRect(this.pos.x, this.pos.y, this.dim.w, this.dim.h);
